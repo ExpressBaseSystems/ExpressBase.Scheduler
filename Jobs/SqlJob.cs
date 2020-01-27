65810cbcb35dea9430f92a18c2cddf71c525114e
Dona Jose <donajose@expressbase.com>
2020-01-27 11:55:57 +0530

    Sql Task

--- Jobs/SqlJob.cs
+++ Jobs/SqlJob.cs
@@ -1,4 +1,5 @@
 ﻿using ExpressBase.Common.Structures;
+using ExpressBase.Objects.ServiceStack_Artifacts;
 using Quartz;
 using ServiceStack;
 using System;
@@ -8,13 +9,22 @@ using System.Threading.Tasks;
 
 namespace ExpressBase.Scheduler.Jobs
 {
-    public class SqlTask:IJob
+    public class SqlTask : EbJob, IJob
     {
         public Task Execute(IJobExecutionContext context)
         {
             JobDataMap dataMap = context.MergedJobDataMap;
             EbJobArguments jobArgs = dataMap["args"] as EbJobArguments;
-         
+
+            MessageProducer.Publish(new SqlJobInternalRequest
+            {
+                GlobalParams = jobArgs.Params,
+                ObjId =jobArgs.ObjId,
+                SolnId = jobArgs.SolnId,
+                UserId = jobArgs.UserId,
+                UserAuthId = jobArgs.UserAuthId
+            });
+
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("Sql Job queued");
             Console.ForegroundColor = ConsoleColor.White;
